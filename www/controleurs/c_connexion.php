5d52f80dadb3147bdc529370ae97aed21b210bd1
Andre RUPIED <andre.rupied@lycee-basch.fr>
2024-04-11 17:55:22 +0200

    correction erreur

--- www/controleurs/c_connexion.php
+++ www/controleurs/c_connexion.php
@@ -12,28 +12,31 @@ switch($action){
 		$mdp = lireDonneePost('mdp');
 		$check = $pdo->getInfosVisiteurHash($login);
 		if(!password_verify($mdp,$check['mdp'])){
-		$hash_mdp = password_hash($mdp,  PASSWORD_ARGON2I);
-		$visiteur = $pdo->getInfosVisiteur($login,$hash_mdp);
-		if(!password_verify($mdp ,$hash_mdp)){
 			ajouterErreur("Login ou mot de passe incorrect", $tabErreurs);
-		  include("vues/v_debutContenu.php");
+			include("vues/v_debutContenu.php");
 			include("vues/v_erreurs.php");
 			include("vues/v_connexion.php");
-		}
-		else{
-			$visiteur = $pdo->getInfosVisiteur($login,$check['mdp']);
-			$id = $visiteur['id'];
-			$nom =  $visiteur['nom'];
-			$prenom = $visiteur['prenom'];
-			connecter($id,$nom,$prenom);
-            include("vues/v_sommaire.php");
-		    include("vues/v_debutContenu.php");
-			include("vues/v_accueil.php");
+		} else {
+			$visiteur = $pdo->getInfosVisiteur($login, $check['mdp']);
+			if(!$visiteur) {
+				ajouterErreur("Login ou mot de passe incorrect", $tabErreurs);
+				include("vues/v_debutContenu.php");
+				include("vues/v_erreurs.php");
+				include("vues/v_connexion.php");
+			} else {
+				$id = $visiteur['id'];
+				$nom = $visiteur['nom'];
+				$prenom = $visiteur['prenom'];
+				connecter($id, $nom, $prenom);
+				include("vues/v_sommaire.php");
+				include("vues/v_debutContenu.php");
+				include("vues/v_accueil.php");
+			}
 		}
 		break;
 	}
 	default :{
-        deconnecter();
+		deconnecter();
 		include("vues/v_debutContenu.php");
 		include("vues/v_connexion.php");
 		break;
